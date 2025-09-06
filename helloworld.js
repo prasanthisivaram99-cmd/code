1fd65ebef75ad234c6c746398d94884673a34835
prasanthi <prasanthisivaram99@gmail.com>
2025-09-06 19:16:55 +0530

    print hello world

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+const createHelloWorld = function () {
+    return function () {
+        return "Hello World"
+
+    }
+    const a = createhelloworld
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
